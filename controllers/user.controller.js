6fb6b63301877b6810907b494f3adebee4e55782
momen-143432443 <momenzo12122001@gmail.com>
2025-02-02 19:07:29 +0300

    Server updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,10 +1,11 @@
 const UserService = require('../services/user.service');
 
 
-exports.register = async(req,res)=>{
+exports.register = (req,res)=>{
     try{
     const {email,password,firstName,lastName} = req.body;
-     await UserService.registerUser(email,password,firstName,lastName);
+      UserService.registerUser(email,password,firstName,lastName);
+     res.setHeader('Content-Type', 'application/json');
     res.json({status:true,success:"user registered success"});
     }catch(e){
         throw e;
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const app = require('./app');
 const db = require('./config/db');
 const UserModel = require('./models/user.model');
+const cors = require('cors');
+const express = require('express');
 
 
 
 const port =  3000;
-    
 
+app.use(express.json());
+app.use(cors());
 app.get('/',(req,res)=>{        
         res.send("Connection success");
     });
